17cc0f727ac3aa8d3162cc5e3cc5263a1d940008
Alfin Afriansyah <143513533+alfinafriansyah@users.noreply.github.com>
2023-10-03 17:21:42 +0700

    Add files via upload

---
+++ Latihan103.java
@@ -0,0 +1,20 @@
+package Jobsheet6;
+
+public class Latihan103 {
+    public static void main(String[] args) {
+        int bil1 = 28, bil2 = 54, bil3 = 15;
+        if (bil1 >= bil2) {
+            if (bil1 >= bil3) {
+                System.out.println("Bilangan terbesar: " + bil1);
+            } else {
+                System.out.println("Bilangan terbesar: " + bil3);
+            }
+        } else {
+            if (bil2 >= bil3) {
+                System.out.println("Bilangan terbesar: " + bil2);
+            } else {
+                System.out.println("Bilangan terbesar: " + bil3);
+            }
+        }
+    }
+}
