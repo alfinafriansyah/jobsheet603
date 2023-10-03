f27887712c9f1f2c78d399e00297fa8b5475747f
Alfin Afriansyah <143513533+alfinafriansyah@users.noreply.github.com>
2023-10-03 11:36:17 +0700

    Add files via upload

---
+++ Pemilihan2Percobaan103.java
@@ -0,0 +1,17 @@
+package Jobsheet6;
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan103 {
+    public static void main(String[] args) {
+        Scanner input03 = new Scanner(System.in);
+        System.out.print("Masukkan tahun: ");
+        int tahun = input03.nextInt();
+        if((tahun % 4) == 0){
+            if((tahun % 100) != 0){
+                System.out.println("Tahun Kabisat");
+            }
+        }else{
+            System.out.println("Bukan Tahun Kabisat");
+        }
+    }
+}
